e5211083ab75d4b0f592a232139a9d6ef3844917
Lubov May <liubov.mayorova@gmail.com>
2021-09-28 14:33:28 -0700

    Moved List, Todos, Golas components to components folder. Updated the App Component.

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,34 @@
 import React from 'react'
+import Todos from './Todos'
+import Goals from './Goals'
+import { connect } from 'react-redux'
+import { handleInitialData } from '../actions/shared'
 
-function App() {
-  return (
-    <div className="App">
-      Hello World
-    </div>
-  );
+
+class App extends React.Component {
+  componentDidMount() {
+    store.dispatch(handleInitialData())
+
+    store.subscribe(() => this.forceUpdate())
+  }
+  render() {
+    const { loading } = store.getState()
+
+    if (loading === true) {
+      return <h3>Loading</h3>
+    }
+
+    return (
+      <div>
+        <Todos />
+        <Goals />
+      </div>
+    )
+  }
 }
 
-export default App;
+export default connect((state) => ({
+  loading: state.loading
+}))(App)
+
+
